373a81aafe8a34e1851e5ef8382c311bdbfdd137
Matias Escujuri <matias.escujuri@hotmail.com>
2026-05-27 21:25:15 -0300

    Primer commit proyecto barcoUade

--- src/main/java/org/barcoUade/BarcoEnemigo.java
+++ src/main/java/org/barcoUade/BarcoEnemigo.java
@@ -1,4 +1,37 @@
 package org.barcoUade;
 
 public class BarcoEnemigo {
+
+
+    private double posicionX; // izquierda y derecha
+    private String direccion;
+    private double velocidadMovimiento;
+
+
+    public void barcoEnemigo(){
+        posicionX = 0;
+        direccion = "derecha";
+        velocidadMovimiento = 2.0;
+    }
+
+
+    public void mover(){
+        if(direccion.equals("derecha")) {
+            posicionX = posicionX + velocidadMovimiento;
+        }else{
+            posicionX = posicionX - velocidadMovimiento;
+
+        }
+        System.out.println("barco moviendose");
+
+    }
+
+
+    public CargaProfundidad lanzarCarga(){
+        System.out.println("barco lanzo una carga");
+
+        return new CargaProfundidad(posicionX);
+
+    }
+
 }
--- src/main/java/org/barcoUade/CargaProfundidad.java
+++ src/main/java/org/barcoUade/CargaProfundidad.java
@@ -1,4 +1,50 @@
 package org.barcoUade;
+import java.util.Random;
 
 public class CargaProfundidad {
+
+    private double posicionX;
+    private double profundidadActual;
+    private double ProfundidadExplosion;
+
+    private double velocidadCaida;
+
+    private boolean exploto;
+
+    public CargaProfundidad(double posicionX) {
+        this.posicionX = posicionX;
+        this.profundidadActual = 0;
+        this.velocidadCaida = 0;
+        this.exploto = false;
+
+
+        generarProfundidadExplosion();
+
+        System.out.println("Carga creadaaa");
+
+
+    }
+
+    public void caer() {
+
+        profundidadActual += velocidadCaida;
+        System.out.println("Carga en caida. profundida actual :" + profundidadActual);
+
+    }
+
+    public void explotar() {
+        if(profundidadActual >= ProfundidadExplosion)
+        exploto = true;
+        System.out.println("Carga explota en profundidad " + ProfundidadExplosion);
+
+    }
+
+
+
+    public void generarProfundidadExplosion() {
+        Random random = new Random();
+
+        ProfundidadExplosion = random.nextInt(401) + 300;
+    }
 }
+
