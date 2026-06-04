64b20b0e6d34e9e0aec64ee6ad8b85b08714bd72
Matias Escujuri <matias.escujuri@hotmail.com>
2026-06-04 10:21:50 -0300

    Primer commit proyecto barcoUade

--- src/main/java/org/barcoUade/VentanaJuego.java
+++ src/main/java/org/barcoUade/VentanaJuego.java
@@ -8,6 +8,10 @@ import javax.swing.JPanel;
 public class VentanaJuego extends JFrame {
 
     private ControladorJuego controlador;
+    private JLabel titulo;
+    private JLabel lblNivel;
+    private JLabel lblPuntaje;
+    private JLabel lblVidas;
 
     public VentanaJuego() {
         controlador = new ControladorJuego();
@@ -19,15 +23,28 @@ public class VentanaJuego extends JFrame {
 
         JPanel panel = new JPanel();
 
-        JLabel titulo = new JLabel("Barco UADE");
-        JButton botonIniciar = new JButton("Iniciar juego");
+        titulo = new JLabel("Barco UADE");
+
+        lblNivel = new JLabel("Nivel: 0");
+        lblPuntaje = new JLabel("Puntaje: 0");
+        lblVidas = new JLabel("Vidas: 0");
+        JButton botonIniciar = new JButton("START GAME");
 
         botonIniciar.addActionListener(e -> {
             controlador.iniciarJuego();
-            titulo.setText("Juego iniciado - Nivel: " + controlador.getNivelActual());
+            titulo.setText("Juego iniciado");
+
+            lblNivel.setText("Nivel: " + controlador.getNivelActual());
+            lblPuntaje.setText("Puntaje: " + controlador.getPuntaje());
+            lblVidas.setText("Vidas: " + controlador.getVidasSubmarino());
         });
 
         panel.add(titulo);
+        panel.add(botonIniciar);
+        panel.add(titulo);
+        panel.add(lblNivel);
+        panel.add(lblPuntaje);
+        panel.add(lblVidas);
         panel.add(botonIniciar);
 
         add(panel);
